49d6f5d7b23f17f33982c2607ffbeafa39fa0988
DaveDMS <dave@digitalmediastream.co.uk>
2025-03-05 14:33:13 +0000

    Creating POST route

--- index.js
+++ index.js
@@ -28,7 +28,15 @@ app.get('/update-cobj', async (req, res) => {
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
 // * Code for Route 3 goes here
-// app.post('/update-cobj');
+app.post('/update-cobj', async (req, res) => {
+    const update = {
+        properties: {
+            "name": req.body.name,
+            "genre": req.body.genre,
+            "rating": req.body.rating,
+        }
+    }
+});
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
