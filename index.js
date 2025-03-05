0f4a6586c3f38c5ddfe8ecbc303919967399dbb7
DaveDMS <dave@digitalmediastream.co.uk>
2025-03-05 14:41:29 +0000

    Add URL & headers for homepage route

--- index.js
+++ index.js
@@ -13,6 +13,13 @@ const PRIVATE_APP_ACCESS = '';
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 // * Code for Route 1 goes here
+app.get('/', async (req, res) => {
+    const movies = 'https://api.hubspot.com/crm/v3/objects/p49464792_movies';
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    }
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
