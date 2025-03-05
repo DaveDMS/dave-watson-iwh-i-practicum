a619e0a1f9f9bc45154a447370447ab41427806b
DaveDMS <dave@digitalmediastream.co.uk>
2025-03-05 14:13:14 +0000

    Updating routes

--- index.js
+++ index.js
@@ -17,11 +17,19 @@ const PRIVATE_APP_ACCESS = '';
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
 // * Code for Route 2 goes here
+app.get('/update-cobj', function(res, req) {
+    try {
+        res.render('contacts', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum' });      
+    } catch (error) {
+        console.error(error);
+    }
+
+});
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
 // * Code for Route 3 goes here
-app.get('/update-cobj');
+// app.post('/update-cobj');
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
